a4e18e192145928ac75eb87829bdff51abf5bad0
Trent Thompson <trent.tt@gmail.com>
2021-01-28 16:55:27 +1100

    working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_072906) do
+ActiveRecord::Schema.define(version: 2021_01_27_231828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_01_27_072906) do
   create_table "users", force: :cascade do |t|
     t.text "email"
     t.string "password_digest"
+    t.text "name"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
 User.destroy_all
-User.create :email => 'trent@trent.com', :password => 'chicken'
-User.create :email => 'bec@trent.com', :password => 'chicken'
+User.create :email => 'trent@trent.com', :password => 'chicken', :name => 'Trent'
+User.create :email => 'bec@trent.com', :password => 'chicken', :name => "Bec"
 puts "#{ User.count } users."
 
 Destination.destroy_all
-Destination.create(:name => 'Boydtown Beach', :location => 'Sapphire Coast', :activity => "Beaches, Fishing", :familyf => 'Great for the kids')
-Destination.create(:name => 'Pound Bend', :location => 'Warrandyte State Park', :activity => "Lilos, Camping, Picnic", :familyf => 'Indeed')
+Destination.create(:name => 'Boydtown Beach', :location => 'Sapphire Coast', :activity => "Beaches, Fishing", :familyf => 'Great for the kids',:image => 'https://media-cdn.tripadvisor.com/media/photo-s/1a/14/13/fc/sunrise.jpg')
+Destination.create(:name => 'Pound Bend', :location => 'Warrandyte State Park', :activity => "Lilos, Camping, Picnic", :familyf => 'Indeed', :image => 'https://www.bushwalkingblog.com.au/wp-content/uploads/2009/05/poundbend.jpg')
 puts "#{ Destination.count } destinations."
+Destination.create(:name => 'Ocean Beach Holiday Resort', :location => 'Umina Beach', :activity => 'Splashaway Bay Waterpark, Adventure Playground, Bike/Buggy Hire', :familyf => 'Heaps of fun family stuff to do', :image => 'https://media-cdn.tripadvisor.com/media/photo-s/0f/60/1f/60/photo6jpg.jpg')
 
 Comment.destroy_all
-Comment.create(:name => 'Jimbo Jones', :blog => 'Such a beautiful location. Great for the kids', :date => '2019-07-22' )
-Comment.create(:name => 'Ray Finckle', :blog => 'The laces were out', :date => '2016-07-22' )
+Comment.create(:name => 'Trent', :blog => 'Such a beautiful location. Great for the kids', :date => '2019-07-22' )
+Comment.create(:name => 'Bec', :blog => 'The laces were out', :date => '2016-07-22' )
 puts "#{ Comment.count } comments."
