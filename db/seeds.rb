0dd8ac9572b78c10cbb81e2c8933218e4afe4d92
Trent Thompson <trent.tt@gmail.com>
2021-01-29 11:17:11 +1100

    changed controls

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@ puts "#{ User.count } users."
 Destination.destroy_all
 Destination.create(:name => 'Boydtown Beach', :location => 'Sapphire Coast', :activity => "Beaches, Fishing", :familyf => 'Great for the kids',:image => 'https://media-cdn.tripadvisor.com/media/photo-s/1a/14/13/fc/sunrise.jpg')
 Destination.create(:name => 'Pound Bend', :location => 'Warrandyte State Park', :activity => "Lilos, Camping, Picnic", :familyf => 'Indeed', :image => 'https://www.bushwalkingblog.com.au/wp-content/uploads/2009/05/poundbend.jpg')
-puts "#{ Destination.count } destinations."
+
 Destination.create(:name => 'Ocean Beach Holiday Resort', :location => 'Umina Beach', :activity => 'Splashaway Bay Waterpark, Adventure Playground, Bike/Buggy Hire', :familyf => 'Heaps of fun family stuff to do', :image => 'https://media-cdn.tripadvisor.com/media/photo-s/0f/60/1f/60/photo6jpg.jpg')
+puts "#{ Destination.count } destinations."
 
 Comment.destroy_all
 Comment.create(:name => 'Trent', :blog => 'Such a beautiful location. Great for the kids', :date => '2019-07-22' )
